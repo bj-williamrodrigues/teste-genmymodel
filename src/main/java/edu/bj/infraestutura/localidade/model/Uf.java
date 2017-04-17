2f86a21cd52433393b52d9167848a18fb581b488
William César Rodrigues <william.rodrigues@bomjesus.br>
2017-04-17 15:06:36 -0300

    Teste localidade

--- src/main/java/edu/bj/infraestutura/localidade/model/Uf.java
+++ src/main/java/edu/bj/infraestutura/localidade/model/Uf.java
@@ -1,11 +1,15 @@
 package edu.bj.infraestrutura.localidade.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
 import javax.persistence.FetchType;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
@@ -32,6 +36,7 @@ public class Uf extends LocalGeografico {
 	private Regiao regiao;
 	
 	private Pais pais;
+	private List<Localidade> localidades = new ArrayList<>();
 	
 	@Column(length = 2)
 	@Size(min = 2, max = 2)
@@ -60,5 +65,14 @@ public class Uf extends LocalGeografico {
 
 	public void setPais(Pais pais) {
 		this.pais = pais;
+	}
+
+	@OneToMany(fetch = FetchType.LAZY, mappedBy = "uf")
+	public List<Localidade> getLocalidades() {
+		return localidades;
+	}
+
+	public void setLocalidades(List<Localidade> localidades) {
+		this.localidades = localidades;
 	}
 }
